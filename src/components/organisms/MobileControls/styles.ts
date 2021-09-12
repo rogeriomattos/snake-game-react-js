66adc0b3a8d8ec0891c031771f172428088b15d4
unknown <rogeriomattos38@outlook.com>
2021-09-12 19:07:46 -0300

    sumindo com os controles mobile

--- src/components/organisms/MobileControls/styles.ts
+++ src/components/organisms/MobileControls/styles.ts
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const MobileControlsContainer = styled.div`
     position: relative;
     padding: 20px;
+    display: none;
+    @media (max-width:700px) {
+        display: block;
+    }
 `;
 
 export const NewGameContainer = styled.div`
